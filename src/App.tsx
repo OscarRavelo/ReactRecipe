1bf744971b8d82f1452c169f3f0e145b0e1b15fb
Oscar Ravelo <oscarravelo@Oscars-MacBook-Air.local>
2023-07-24 23:21:12 +0300

    add tailwind and project architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
+import CardRecipe from './components/CardRecipe'
 
 function App() {
 
   return (
 
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <CardRecipe />
     </>
 
   )
---
+++ src/components/CardRecipe.jsx
@@ -0,0 +1,13 @@
+
+const CardRecope = ({ recipes }) => {
+
+
+
+  return (
+    <div className="bg-gray-500 w-1/2 h-1/2 flex" > Recipe list</div>
+
+  );
+
+}
+
+export default CardRecope;
